7141bb7f7b74ea50241562d93ad953302edaa7a4
박은비 <eunbi628268@gmail.com>
2022-05-09 20:49:30 +0900

    [update] 0509

---
+++ 0509연습.sql
@@ -0,0 +1,22 @@
+
+drop table emp_copy;
+
+create table emp_copy
+as
+select * from employee;
+
+select * from emp_copy;
+
+desc emp_copy;
+
+"Insert into emp_copy (eno, name, job, manager, hiredate, salary, commission , dno) 
+values(eno,'" + name + "' , '" + job + "', manager, hiredate, salary, commission, dno)";
+
+Insert into emp_copy (eno, name, job, manager, hiredate, salary, commission , dno) 
+values(enoo,'" + name + "','" + job + "', managerr + "','"+ to_date(hiredate,'yyyy-mm-dd') + "', salaryy  , commissionn , dnoo);
+
+Insert into emp_copy (eno, name, job, manager, hiredate, salary, commission , dno) 
+values(enoo,'" + name + "','" + job + "', managerr , to_date(hiredate,'yyyy-mm-dd'), salaryy  , commissionn , dnoo)";
+
+Insert into emp_copy (eno, name, job, manager, hiredate, salary, commission , dno) 
+values(enoo,'" + name + "','" + job + "', managerr , '" + hiredate + "', salaryy  , commissionn , dnoo)"
---
+++ 회원테이블생성_db연결.sql
@@ -0,0 +1,34 @@
+-- 회원 정보를 저장하는 테이블
+Create table mbTbl (
+    idx number not null,  -- 시퀀스 부착, 증가 컬럼
+    id varchar2(100) not null,
+    pass varchar2(100) not null,
+    name varchar2(100) not null,
+    email varchar2(100) not null,
+    city varchar2(100) null,
+    phone varchar2(100) null);
+    
+select * from mbtbl;
+
+-- id 에 primary키 장착
+alter table mbTbl
+add constraint mbTbl_id_PK PRIMARY KEY (id);
+
+desc mbtbl;
+
+-- idx에 시퀀스 장착
+create SEQUENCE seq_mbTbl_idx
+    increment by 1
+    start with 1
+    nocache;
+    
+/* 더미 데이터 입력 */
+insert into mbTbl (idx, id, pass,name, email, city, phone)
+values (seq_mbTbl_idx.nextval,'admin', '1234', '관리자' , 'kosmo@kosmo.com','Seoul','010-2055-6828');
+
+Insert into mbTbl (idx ,id , pass, name, email) values (seq_mbTbl_idx.nextval, '" + id + "','" + passwd + "','" + name + "','" + email + "')";
+
+
+
+
+    
