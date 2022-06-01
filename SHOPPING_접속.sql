571a8c34b4acc6b9fa4e4b13f301581df7f91962
박은비 <eunbi628268@gmail.com>
2022-06-01 17:00:49 +0900

    [update] 0601

--- SHOPPING_접속.sql
+++ SHOPPING_접속.sql
@@ -1,6 +1,77 @@
 create table product (
     id number(10) primary key,
     category varchar2(10) not null,
+    wname		varchar2(20) not null,
+    pname		varchar2(20) not null,
+    sname		varchar2(20) not null,
+    price		number(10) default 0,
+    downprice	number(10) default 0,
+    inputdate	varchar2(20) default sysdate,
+    stock		number(10) default 0,
+    description	varchar2(2000),
+    small		varchar2(20),
+    large		varchar2(20)
+);
+
+create table member (
+    id varchar2(10) not null primary key, 
+    pass varchar2(20) not null,
+    name varchar2(20) not null,
+    regidate date default sysdate not null,
+    address varchar2(500) not null,
+    email varchar2(20) not null
+    );
+select * from member;
+
+create table saleorder(
+    id		number(10) primary key,
+    name		varchar2(20) not null,
+    orderdate	varchar2(20) not null,
+    addr		varchar2(50) not null,
+    tel		varchar2(20) not null,
+    pay		varchar2(10) not null,
+    cardno		varchar2(20) not null,
+    prodcount	NUMBER(20) default 0,
+    total		NUMBER(20) default 0
+);
+
+drop table board;
+
+create sequence seq_board_peb
+    increment by 1
+    start with 1
+    nocache;
     
 
-)
+create table board(
+    num varchar2(10),
+    id varchar2(10) ,
+    name VARCHAR2(50) not null,
+    title VARCHAR2(200) not null,
+    content  VARCHAR2(2000) not null,
+    postdate date DEFAULT sysdate not null,
+    ofile VARCHAR2(200),
+    sfile VARCHAR2(50),
+    downcount number(5) DEFAULT 0 not null,
+    pass VARCHAR2(50) not null,
+    visitcount number DEFAULT 0 not null
+);
+select * from board;
+
+alter table board
+add constraint BOARD_ID_FK Foreign Key (id) REFERENCES member(id);
+
+desc board;
+
+select * from user_constraints
+where table_name = 'BOARD';
+
+insert into board ( num, id, name, title,content ,postdate, downcount, pass, visitcount)
+values (seq_board_peb.nextval,'20170033','박은비', '가나다라', '내용' , default, default,'1234', default); 
+
+insert into board ( num, id, name, title,content ,postdate, downcount, pass, visitcount)
+values (seq_board_peb.nextval,'201700','박은', '가나다라', '내용' , default, default,'1234', default); 
+
+insert into member
+values ('20170033', '1234', '박은비', default, '서울특별시', 'eun628268@gmail.com');
+commit;
