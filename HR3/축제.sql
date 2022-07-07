3e6739346e25161b7ab829e6b8ce919e7fbfb2bb
Eunbi Park <eunbi628268@gmail.com>
2022-07-07 16:17:03 +0900

    [update] 축제 테이블 생성, 수정

--- HR3/축제.sql
+++ HR3/축제.sql
@@ -1,10 +1,9 @@
-select * from festival;
+select * from festival order by f_code;
+-- f_code primary≈∞ √ﬂ∞°
+alter table festival add constraint pk_f_code primary key (f_code);
 
 delete festival where f_ny is null or  f_nx is null;
-
-select * 
-from festival;
-
 select count(*) from festival;
 
 commit;
+
