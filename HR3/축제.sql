ea9a5fa9df2908a1a9e34ce9b5d0ca02c57a4f76
Eunbi Park <eunbi628268@gmail.com>
2022-07-08 23:55:03 +0900

    [update] 0708

--- HR3/축제.sql
+++ HR3/축제.sql
@@ -1,4 +1,6 @@
-select * from festival order by f_code;
+drop table festival;
+
+select * from festival order by f_code, f_startdate;
 -- f_code primaryÅ° Ãß°¡
 alter table festival add constraint pk_f_code primary key (f_code);
 
@@ -6,4 +8,11 @@ delete festival where f_ny is null or  f_nx is null;
 select count(*) from festival;
 
 commit;
+
+SELECT F_CODE, F_NAME, F_PLACE, F_STARTDATE, F_ENDDATE, F_TEL, F_ADMIN, F_NY, F_NX FROM ( SELECT 
+F_CODE, F_NAME, F_PLACE, F_STARTDATE, F_ENDDATE, F_TEL, F_ADMIN, F_NY, F_NX, ROW_NUMBER() OVER(ORDER 
+BY F_CODE) AS RNUM FROM FESTIVAL WHERE 1 = 1 AND TO_NUMBER(F_STARTDATE,?'YYYY')?>?2023 
+) FE WHERE RNUM BETWEEN 1 AND 10 ORDER BY F_CODE ;
+
+select * from festival where  '2022-05-01' < F_STARTDATE ORDER BY F_STARTDATE ;
 
